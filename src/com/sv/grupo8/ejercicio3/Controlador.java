368dbe457f020493f61d8fbbbe33ed163f0b7c0e
Amilcar Crespin <alexandercrespin476@gmail.com>
2026-07-25 16:02:59 -0600

    Agrego clase Controlador

--- src/com/sv/grupo8/ejercicio3/Controlador.java
+++ src/com/sv/grupo8/ejercicio3/Controlador.java
@@ -1,5 +1,41 @@
 package com.sv.grupo8.ejercicio3;
 
 public class Controlador {
+    private String numeroCuenta;
+    private double saldo;
 
+    public Controlador(String numeroCuenta, double saldo) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void aplicarInteresAnual(double porcentaje) {
+        double tiempo = 8.0 / 12.0;
+        double interes = saldo * porcentaje * tiempo;
+        saldo = saldo + interes;
+    }
+}
+
+class ControladorTest {
+    public static void main(String[] args) {
+        Controlador cuenta = new Controlador("001", 1000);
+        cuenta.aplicarInteresAnual(0.05);
+        System.out.println("Saldo final: " + cuenta.getSaldo());
+    }
 }
