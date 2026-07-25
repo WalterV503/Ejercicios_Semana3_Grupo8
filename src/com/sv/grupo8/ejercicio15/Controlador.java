1c4a84d06d6373cc9f389d515a656c6fd64f14d6
Amilcar Crespin <alexandercrespin476@gmail.com>
2026-07-25 16:27:29 -0600

    agregue la superclase Vehiculo y subclase Camion con herencia

--- src/com/sv/grupo8/ejercicio15/Controlador.java
+++ src/com/sv/grupo8/ejercicio15/Controlador.java
@@ -1,5 +1,11 @@
 package com.sv.grupo8.ejercicio15;
 
 public class Controlador {
+    public static void main(String[] args) {
+        Camion camion = new Camion("P123-456", "Volvo", 3);
 
+        System.out.println("Placa: " + camion.getPlaca());
+        System.out.println("Marca: " + camion.getMarca());
+        System.out.println("Numero de ejes: " + camion.getNumeroEjes());
+    }
 }
---
+++ src/com/sv/grupo8/ejercicio15/vehiculo.java
@@ -0,0 +1,20 @@
+package com.sv.grupo8.ejercicio15;
+
+
+public class vehiculo {
+    private String placa;
+    private String marca;
+
+    public vehiculo(String placa, String marca) {
+        this.placa = placa;
+        this.marca = marca;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+}
